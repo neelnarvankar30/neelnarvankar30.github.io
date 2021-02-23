0bb589f31bd7fef746c6785caf7d0a3851d0deae
Neel Narvankar <neelnarvankar30@gmail.com>
2021-02-22 19:42:21 -0500

    reactjstest

---
+++ src/components/ContactUs.js
@@ -0,0 +1,30 @@
+import React, { Component } from 'react';
+export default class ContactUs extends Component {
+  render() {
+    let resumeData = this.props.resumeData;
+    return (
+      <section id="contact">
+          <div className="row section-head">
+            <div className="ten columns">
+              <p className="lead">
+              Feel free to contact me<hr/>
+              </p>
+            </div>
+          </div>
+          <div className="row">
+            <aside className="eigth columns footer-widgets">
+              <div className="widget">
+                <h4>  Email: 
+                  <span/> { resumeData.email}
+
+                </h4>
+                <h4>Linked in:
+                   <span></span> {resumeData.linkedinId}
+                </h4>
+              </div>
+            </aside>
+          </div>
+        </section>
+        );
+  }
+}
